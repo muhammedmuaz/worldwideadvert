c79126a0be585cc565c1adc4f3f6342c18a4e26f
unknown <syedfareed667@gmail.com>
2023-05-18 15:01:08 +0500

    app drawer

--- lib/Screens/widgets/drawer.dart
+++ lib/Screens/widgets/drawer.dart
@@ -1,22 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:worldwideadverts/Screens/widgets/my_drawer_header.dart';
-import 'package:worldwideadverts/Screens/widgets/my_drawer_list.dart';
+import 'package:get/get.dart';
+
+import 'package:worldwideadverts/Screens/widgets/my_drawer_item.dart';
+
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-        child: SingleChildScrollView(
-      child: Container(
-        child: Column(
+    return SafeArea(      
+      child: Scaffold(
+        backgroundColor:const Color(0xff05BFDB),
+        body: ListView(
+          padding:const  EdgeInsets.symmetric(horizontal: 12.0),
           children: [
-            const MyDrawerHeader(),
-            drawerList(),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(onPressed: (){
+                  Get.back();
+                }, icon:const  Icon(Icons.menu)),
+                const SizedBox(
+                  height: 25.0,
+                ),
+                const CircleAvatar(
+                  backgroundImage: NetworkImage("https://images.pexels.com/photos/4709285/pexels-photo-4709285.jpeg?auto=compress&cs=tinysrgb&w=600"),
+                  radius: 30.0,
+                ),                           
+              ],
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+
+        myDrawerItem("Home", Icons.home),
+        myDrawerItem("Categories", Icons.category),
+        myDrawerItem("My Order", Icons.category),
+        myDrawerItem("Favourite List", Icons.favorite),
+
+        const Divider(height: 5,color: Colors.white,),
+        const SizedBox(
+              height: 15.0,
+            ),
+        
+
+       myDrawerItem("Rating & Review", Icons.home),
+        myDrawerItem("My Wallet", Icons.home),
+        myDrawerItem("Settings", Icons.home),
+
+        const Divider(color:  Colors.white,),
+        const SizedBox(
+              height: 15.0,
+            ),
+               myDrawerItem("Logout", Icons.home),       
+            
+            
           ],
         ),
       ),
-    ));
+    );
   }
+
 }
+
--- lib/Screens/widgets/my_drawer_item.dart
+++ lib/Screens/widgets/my_drawer_item.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
-Widget myDrawerItem(IconData icon, String title) {
+Widget myDrawerItem(String title, IconData icon) {
   return Material(
+    color: Colors.transparent,
     child: InkWell(
       onTap: () {},
       child: Padding(
-        padding: const EdgeInsets.all(5.0),
+        padding: const EdgeInsets.only(bottom: 15.0),
         child: Row(
           children: [
             Icon(
               icon,
               size: 16.0,
-              color: Colors.black,
+              color: Colors.white,
             ),
             const SizedBox(
               width: 20.0,
             ),
             Text(
               title,
-              style: const TextStyle(fontSize: 16.0, color: Colors.black),
+              style: const TextStyle(fontSize: 16.0, color: Colors.white),
             ),
           ],
         ),
